5cecdd8dc087fdb43cca23ccf45abf9815e549c6
Jeffrey Varner <jdv27@cornell.edu>
2023-09-20 04:49:27 -0400

    Update Types.jl
    small change to type

--- src/Types.jl
+++ src/Types.jl
@@ -184,9 +184,9 @@ mutable struct MyUSTreasuryCouponSecurityModel <: AbstractTreasuryDebtSecurity
 
     # data -
     par::Float64                                    # Par value of the bill
-    rate::Float64                                   # Annualized effective interest rate
-    coupon::Float64                                 # Coupon rate
-    T::Float64                                      # Duration in years, measured as a 365 day or a 52 week year
+    rate::Union{Nothing, Float64}                   # Annualized effective interest rate
+    coupon::Union{Nothing, Float64}                 # Coupon rate
+    T::Union{Nothing,Float64}                       # Duration in years, measured as a 365 day or a 52 week year
     λ::Int                                          # Number of coupon payments per year (typcially 2)
     price::Union{Nothing, Float64}                  # Price of the bond or note
     cashflow::Union{Nothing, Dict{Int,Float64}}     # Cashflow
