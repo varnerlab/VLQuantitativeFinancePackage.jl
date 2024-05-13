a9dcb309b29c1e9736e3db4bf682e1f9e9663406
Jeffrey Varner <jdv27@cornell.edu>
2024-05-12 21:39:49 -0400

    Update EulerMaruyamaMethod.jl
    Heston model

--- src/EulerMaruyamaMethod.jl
+++ src/EulerMaruyamaMethod.jl
@@ -2,7 +2,50 @@
 function _solve(model::MyHestonModel, tspan::NamedTuple,
     initialconditions::AbstractArray, N::Int64, method::EulerMaruyamaMethod)::Tuple
     
-    return (0,0);
+    # initialize -
+    d = model.μ
+    f = model.κ
+    g = model.θ
+    h = model.ξ
+    Σ = model.Σ
+    Xₒ = initialconditions;
+
+    # build the time and state arrays -
+    tₒ = tspan.start;
+    tₙ = tspan.stop;
+    dt = tspan.step;
+    T = range(tₒ, step=dt, stop=tₙ) |> collect
+    M = length(T) # how many time steps do we have?
+    X = Array{Float64,2}(undef, M, N) # initialize an empty array to store the price paths
+    V = Array{Float64,2}(undef, M, N) # initialize an empty array to store the price paths
+
+    # populate the first row -
+    foreach(p -> X[1,p] = Xₒ[1], 1:N)
+    foreach(p -> V[1,p] = Xₒ[2], 1:N)
+
+    # pre generate the noise matrix -
+    ZM = MvNormal(zeros(2), Σ);
+
+    for p ∈ 1:N
+        for i ∈ 2:M
+            
+            # call parameter functions 
+            μ = d(X[i-1,p],T[i]) # drift
+            κ = f(X[i-1,p],T[i]) # diffusion
+            θ = g(X[i-1,p],T[i]) # mean reversion
+            ξ = h(X[i-1,p],T[i]) # volatility of volatility
+
+            # generate a sample -
+            Z = rand(ZM);
+
+            # update the state equations -
+            X[i,p] = X[i-1,p] + (μ*X[i-1,p])*dt + sqrt(V[i-1,p])*X[i-1,p]*(sqrt(dt)*Z[1]);
+            V[i,p] = V[i-1,p] + κ*(θ - V[i-1,p])*dt + ξ*(sqrt(V[i-1,p]))*(sqrt(dt)*Z[2]);
+        end
+    end
+
+    # return the time and state arrays -
+    return (T, X, V);
 end
 
 
