f304af0753575939ca0023f50128c085ce474f35
Jeffrey Varner <jdv27@cornell.edu>
2024-05-23 11:44:32 -0400

    Update Hippo.jl
    some randomness

--- src/Hippo.jl
+++ src/Hippo.jl
@@ -61,7 +61,7 @@ function prediction(model::MySisoLegSHippoModel, tspan::NamedTuple, signal::Arra
             
             # reset the hidden states -
             for k ∈ 1:number_of_hidden_states
-                X[i,k] = X[j,k]; # jump back to the last stable state
+                X[i,k] = X[j,k]*(1+0.25*randn()); # jump back to the last stable state
             end
         end
     end
