28e2f071e8d77f756f7e980787d45e5db32f1b8c
Jeffrey Varner <jdv27@cornell.edu>
2024-07-23 19:00:17 -0400

    new
    new state

--- src/Factory.jl
+++ src/Factory.jl
@@ -801,7 +801,7 @@ function build(modeltype::Type{MyPeriodicRectangularGridWorldModel},
     model.moves = moves;
     model.number_of_rows = nrows
     model.number_of_cols = ncols
-    model.stateactionmap = stateactionmap
+    model.stateactionnewstate = stateactionmap
 
     # return -
     return model
